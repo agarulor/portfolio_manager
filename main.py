926fe3a39a855dbfa93efd92cf9261b0390b6514
agarulor <96448726+agarulor@users.noreply.github.com>
2025-11-05 12:41:44 +0100

    We have updated a few files

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from data_management.clean_data import clean_and_align_data
 from data_management.save_data import save_preprocessed_data
 from portfolio_tools.return_metrics import calculate_daily_returns, annualize_returns, portfolio_returns
 from portfolio_tools.risk_metrics import calculate_covariance, portfolio_volatility, sharpe_ratio
+from portfolio_tools.markowitz import minimize_volatility
 if __name__ == "__main__":
     #a = get_stock_prices("data/input/eurostoxx50_csv.csv", "ticker_yahoo", "name",
     #                     start_date = "2024-11-03", end_date = "2025-11-03"
@@ -12,22 +13,12 @@ if __name__ == "__main__":
     #print(b.head())
     #save_preprocessed_data(b)
     e = read_price_file("data/processed/prices_20251105-013748.csv")
-    f = calculate_daily_returns(e, method="log")
+    f = calculate_daily_returns(e, method="simple")
     h = calculate_covariance(f)
     n = 50
 
-    # Genera pesos aleatorios positivos
-    pesos = np.full(35, 0.02)
-
-    # Normaliza para que sumen 1
-    pesos = pesos / pesos.sum()
-    i = portfolio_volatility(pesos, h)
-    print(i)
-
-    j = portfolio_returns(pesos, f, method="log")
-    print(j)
-    sharpe_ratio = sharpe_ratio(j, i)
-    print(sharpe_ratio)
+    n = annualize_returns(f, method="simple")
+    l = minimize_volatility(0.10, n , h)
 
 
 
---
+++ src/portfolio_tools/markowitz.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+from typing import Literal
+from portfolio_tools.return_metrics import portfolio_returns, annualize_returns
+from portfolio_tools.risk_metrics import portfolio_volatility
+from scipy.optimize import minimize
+
+
+
+def minimize_volatility(target_return: float,
+                        returns: pd.DataFrame,
+                        covmat: np.ndarray,
+                        method:  Literal["simple", "log"] = "simple",
+                        periods_per_year=252) -> np.ndarray:
+    """
+    Returns the optimal weight of the portfolio assets that minimize
+    volatility for a given target return, returns and a covariance matrix.
+
+    Parameters
+    ----------
+    target_return : float. Expected return of the portfolio.
+
+    returns: pd.DataFrame. Expected return of the portfolio.
+
+    covmat: np.ndarray. Covariance matrix of the portfolio.
+
+    Returns
+    -------
+    np.ndarray: Optimal weight of the portfolio.
+    """
+
+    annualized_returns = annualize_returns(returns,
+                                           method=method,
+                                           periods_per_year=periods_per_year)
+    # Number of assets in the spectrum of assets
+    n = annualized_returns.shape[0]
+
+    # Initial guess of weights (we start with evenly weights)
+    init_guess = np.ones(n) / n
+    print(n)
+    # We ensure that there is no short-selling (i.e. no short positions)
+    bounds = [(0, 1)] * n
+
+    # We add the constraints to the model
+    # Weights must sum 1 (fully invested)
+    # Portfolio expected return is equal to the target return
+    constraints = (
+        {"type": "eq", "fun": lambda w: np.sum(w) - 1},
+        {"type": "eq", "fun": lambda w: w @ annualized_returns - target_return}
+    )
+    result = minimize(portfolio_volatility, init_guess,
+                       args=(covmat,), method='SLSQP',
+                       options={'disp': False},
+                       constraints=constraints,
+                       bounds=bounds)
+
+    return result.x
+
+
+
