a147671129254ba427206069cd974a022f62b477
agarulor <96448726+agarulor@users.noreply.github.com>
2025-12-26 03:32:02 +0100

    000

--- main.py
+++ main.py
@@ -20,20 +20,7 @@ np.random.seed(SEED)
 
 
 def main():
-
-
-
-
-
-
-    #g = calculate_covariance(train_set)
-
-#h = create_markowitz_table(train_set, test_set, g, rf = 0.000, min_w=0.025, max_w=0.16, custom_target_volatility=0.26)
-
-#print(h)
     run_app()
-#plot_frontier(60, train_set, g,  method="simple", min_w=0.025, max_w=0.20, custom_target_volatility=0.26)
-
 
 if __name__ == "__main__":
     main()
